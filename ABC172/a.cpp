b70f0a5f8d8b97200e83fd6084dd66055b9e9c75
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-06-28 10:54:12 +0530

    Create a.cpp

---
+++ ABC172/a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve(){
+    ll a;
+    cin >> a;
+    ll cnt = a + a*a + a*a*a;
+    cout << cnt<< endl;
+}
+
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  //int t;cin>>t;while(t--)
+  solve();
+  return 0;
+}
